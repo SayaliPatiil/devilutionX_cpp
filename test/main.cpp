2a1b77147aafc28d7bca8b429022b33cecc3a99e
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2022-06-14 12:38:52 +0530

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,18 +1,1 @@
-#include <gtest/gtest.h>
 
-#include "diablo.h"
-#include "options.h"
-
-int main(int argc, char **argv)
-{
-	// Disable error dialogs.
-	devilution::gbQuietMode = true;
-
-#if SDL_VERSION_ATLEAST(2, 0, 0)
-	// Disable hardware cursor while testing.
-	devilution::sgOptions.Graphics.hardwareCursor.SetValue(false);
-#endif
-
-	testing::InitGoogleTest(&argc, argv);
-	
-}
